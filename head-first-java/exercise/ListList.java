8140df11d617ed8cc9a0cdb13d98c68409000636
植木善弘 <pirori67@gmail.com>
2015-12-23 20:28:18 +0900

    ListList added.

---
+++ head-first-java/exercise/ListList.java
@@ -0,0 +1,10 @@
+import java.lang.Integer;
+import java.util.*;
+
+public class ListList {
+    public static void main(String[] args) {
+//        List<String> strings = new ArrayList<>();
+        ArrayList<ArrayList<Integer>> list = new ArrayList<>();
+//        List<List<Map<String, WeakReference<B>>>> list = new ArrayList<>();
+    }
+}
