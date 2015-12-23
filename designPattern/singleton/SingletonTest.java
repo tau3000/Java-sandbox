0385bd081fe62c22fa459d3fa7b8d950faa1b8e5
植木善弘 <pirori67@gmail.com>
2015-12-23 21:50:22 +0900

    same instance checking added.

--- designPattern/singleton/SingletonTest.java
+++ designPattern/singleton/SingletonTest.java
@@ -5,6 +5,10 @@ import java.lang.System;
 public class SingletonTest {
     public void test() {
         System.out.println("Singleton Test");
-        Alone alone = Alone.getInstance();
+        Alone alone1 = Alone.getInstance();
+        Alone alone2 = Alone.getInstance();
+        if(alone1 == alone2){
+            System.out.println("同じクリぼっちでした。");
+        }
     }
 }
