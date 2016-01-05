588e03c06db5d88283a01e1344e3eb75699b6b33
植木善弘 <pirori67@gmail.com>
2016-01-05 17:15:31 +0900

    Multicast phrase added.

--- designPattern/multicast/MulticastTest.java
+++ designPattern/multicast/MulticastTest.java
@@ -5,6 +5,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.lang.System;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -85,7 +86,7 @@ public class MulticastTest extends JPanel implements ActionListener {
     }
 
     public void test() {
-
+        System.out.println("Multicast Test");
         javax.swing.SwingUtilities.invokeLater(new Runnable() {
 
             @Override
