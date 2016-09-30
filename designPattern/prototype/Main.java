95118dd54dd2d170a68c809dca478d92236d73fa
Yoshihiro_Ueki <yoshihiro_ueki@yu.local>
2016-09-30 09:55:03 +0900

    replace register name

--- designPattern/prototype/Main.java
+++ designPattern/prototype/Main.java
@@ -9,22 +9,20 @@ public class Main {
         final AnnounceDateData publishment = new AnnounceDateData("公表");
         final AnnounceDateData observation = new AnnounceDateData("観測");
 
-        dealDataManager.register("買取", acquistionAll);
-        dealDataManager.register("少数持分取得", acquistionPart);
-        dealDataManager.register("公表", publishment);
-        dealDataManager.register("観測", observation);
+        dealDataManager.register("買取データ", acquistionAll);
+        dealDataManager.register("少数持分取得データ", acquistionPart);
+        dealDataManager.register("公表データ", publishment);
+        dealDataManager.register("観測データ", observation);
 
-        DealData d1 = dealDataManager.create("買取");
-        DealData d2 = dealDataManager.create("少数持分取得");
-        DealData d3 = dealDataManager.create("公表");
-        DealData d4 = dealDataManager.create("観測");
+        DealData d1 = dealDataManager.create("買取データ");
+        DealData d2 = dealDataManager.create("少数持分取得データ");
+        DealData d3 = dealDataManager.create("公表データ");
+        DealData d4 = dealDataManager.create("観測データ");
 
         final Print print = new Print();
         print.println(d1.getFormattedData(500000));
         print.println(d2.getFormattedData(1000));
         print.println(d3.getFormattedData(20160919));
         print.println(d4.getFormattedData(20161225));
-
     }
-
 }
