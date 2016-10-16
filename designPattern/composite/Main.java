80d05c42389eca237631cd6fbef594811cdbd529
Yoshihiro_Ueki <yoshihiro_ueki@yu.local>
2016-10-16 13:08:29 +0900

    refactoring

--- designPattern/composite/Main.java
+++ designPattern/composite/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         Directory dir1 = new Directory("temp");
         dir1.add(new File("industries.dat"));
-        Directory dir2 = new Directory("currencyDat");
+        Directory dir2 = new Directory("currency_dat");
         dir2.add(new File("jpn.dat"));
         dir2.add(new File("chn.dat"));
         dir1.add(dir2);
